4fd0c6473b07e797110da8a3a1678bd0bbb19cc4
abolfazl sanandaji <abolfazlsanandaji@gmail.com>
2025-04-06 09:34:39 +0330

    add route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ElementsModule } from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ElementsModule,
+    CollectionsModule,
     AppRoutingModule
   ],
   providers: [],

ee6130a22f8201870ab6461d34b531fd5f02636d
abolfazl sanandaji <abolfazlsanandaji@gmail.com>
2025-04-07 10:50:59 +0330

    collection table finish

--- src/app/collections/collection-home/collection-home.component.ts
+++ src/app/collections/collection-home/collection-home.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './collection-home.component.css'
 })
 export class CollectionHomeComponent {
-
+data=[
+  {name:'reza',age:27 ,job:'ui',employed:false},
+  {name:'ali',age:28 ,job:'billboard',employed:true},
+  {name:'mohamad',age:29 ,job:'tester',employed:true},
+]
+header=[
+  {key:'name',value:'Name',show:true},
+  {key:'age',value:'Age',show:true},
+  {key:'job',value:'Job',show:true},
+  {key:'employed',value:'Employed',show:false},
+]
 }
